dec5fa1a6ad6f5a88614f82ac7376946c0241be2
Nodarski <nodarlaw@gmail.com>
2018-05-02 12:39:51 -0600

    add background image file. minor work on css(header and content alignment)

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//start game
+//player selects character, 4 options
+//selected character becomes main character
+//remaining three become enemies
+
+//each character has 3 attributes:health, attack power and counter attck power
+
+//main character stays in original spot
+
+//enemies move to new section "enemies available to attack"
+//player can choose from the three enemies, which he would like to attack first
+//selected enemy moves to defender posision
+//after enemy is defeated, remove the character
+//player chooses new enemy
+//repeat until no more enemies left = game has been won 
+
+
+
+//character mehanics
+//characters health ranges from 100-180
+//counter attack power between 5 and 45
+//characters counter attack power does not change
+//attack power starts at 8. for every chracter
+//attacking causes attack power ++8
+
