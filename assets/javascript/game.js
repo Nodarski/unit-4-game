d159bb239a2ea09d336dd748ef68f0b21e7736ed
Nodarski <nodarlaw@gmail.com>
2018-05-02 15:43:51 -0600

    add js code for selcting character and enemy, as well as each characters attributes (health attk pwr and counter attack)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,101 @@
 //start game
 //player selects character, 4 options
+var mainChar;
+var currEnemy;
+var luke = {
+    health:100,
+    attkPower:8,
+    counterPower:5
+}
+var obiWan = {
+    health:120,
+    attkPower:8,
+    counterPower:10
+}
+var theSenate = {
+    health:150,
+    attkPower:8,
+    counterPower:20
+}
+var maul = {
+    health:180,
+    attkPower:8,
+    counterPower:45
+}
+
+    $(".charSelect").on("click",".luke", function(){
+        document.getElementById("pickChar").innerHTML = "Your Character!!";
+        $(".obiWan").appendTo(".chooseEnemy");
+        $(".theSenate").appendTo(".chooseEnemy");
+        $(".dMaul").appendTo(".chooseEnemy");
+        mainChar = luke;
+        console.log(mainChar);
+    });
+
+    $(".charSelect").on("click",".obiWan", function(){
+        document.getElementById("pickChar").innerHTML = "Your Character!!";
+        $(".luke").appendTo(".chooseEnemy");
+        $(".theSenate").appendTo(".chooseEnemy");
+        $(".dMaul").appendTo(".chooseEnemy");
+        mainChar = obiWan;
+        console.log(mainChar);
+    });
+
+    $(".charSelect").on("click",".theSenate", function(){
+        document.getElementById("pickChar").innerHTML = "Your Character!!";
+        $(".obiWan").appendTo(".chooseEnemy");
+        $(".luke").appendTo(".chooseEnemy");
+        $(".dMaul").appendTo(".chooseEnemy");
+        mainChar = theSenate;
+        console.log(mainChar);
+    });
+
+    $(".charSelect").on("click",".dMaul", function(){
+        document.getElementById("pickChar").innerHTML = "Your Character!!";
+        $(".obiWan").appendTo(".chooseEnemy");
+        $(".theSenate").appendTo(".chooseEnemy");
+        $(".luke").appendTo(".chooseEnemy");
+        mainChar = maul;
+        console.log(mainChar);
+    });
+
+    
+       
+    
+    $(".chooseEnemy").on("click",".luke", function(){
+        if( !$.trim( $('.enemyChosen').html() ).length ) {
+        $(".luke").appendTo(".enemyChosen");
+        $(".defender").text("Defender!!");
+        currEnemy = luke;
+        }
+    });
+    $(".chooseEnemy").on("click",".obiWan", function(){
+        if( !$.trim( $('.enemyChosen').html() ).length ) {
+        $(".obiWan").appendTo(".enemyChosen");
+        $(".defender").text("Defender!!");
+        currEnemy = obiWan;
+        }
+    });
+    $(".chooseEnemy").on("click",".theSenate", function(){
+        if( !$.trim( $('.enemyChosen').html() ).length ) {
+        $(".theSenate").appendTo(".enemyChosen");
+        $(".defender").text("Defender!!");
+        currEnemy = theSenate;
+        }
+    });
+    $(".chooseEnemy").on("click",".dMaul", function(){
+        if( !$.trim( $('.enemyChosen').html() ).length ) {
+        $(".dMaul").appendTo(".enemyChosen");
+        $(".defender").text("Defender!!");
+        currEnemy = maul;
+        }
+    });
+
+
+
+
+
+
 //selected character becomes main character
 //remaining three become enemies
 
